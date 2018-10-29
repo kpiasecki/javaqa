2b322f62855535445de303f944c9a7744d2f441e
kpi <krzysztof.piasecki@intive.com>
2018-10-29 13:34:18 +0100

    arrays and collections

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,65 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        /*
-        Instrument instrument = new Instrument(InstrumentColour.RED);
-        System.out.println(instrument.getColour());
-        System.out.println(instrument.play());
-        System.out.println();
-        */
+        int []c = {5,4,2};
+        int []d = new int[4];
+        int []e = new int[]{3,5,6};
+        int [][]f = new int[3][4];
+        int [][]g = new int[][]{{1, 2, 3}, {1, 2}, {1}};
 
-        Piano piano = new Piano(InstrumentColour.WHITE);
-        System.out.println(piano);
-        System.out.println(piano.getKind());
-        System.out.println(piano.getColour());
-        System.out.println(piano.play());
+        System.out.println(c[2]);
+        System.out.println(g[1][0]);
         System.out.println();
 
-        Guitar guitar = new Guitar(InstrumentColour.BLUE);
-        System.out.println(guitar);
-        System.out.println(guitar.getKind());
-        System.out.println(guitar.getColour());
-        System.out.println(guitar.play());
+
+        List<Integer> list = new ArrayList<>();
+        list.add(5);
+        list.add(60);
+        list.add(468);
+
+        for(Integer i : list) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+
+        Set<String> set = new HashSet<>();
+        set.add("Luke Skywalker");
+        set.add("Chewbacca");
+        set.add("Obi Wan Kenobi");
+        set.add("Luke Skywalker");
+        set.add("Chewbacca");
+        set.add("Luke Skywalker");
+
+        for(String s : set) {
+            System.out.println(s);
+        }
+
+        System.out.println();
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+
+        for(Integer m : map.keySet()) {
+            System.out.println(m);
+        }
+        System.out.println();
+
+        for(String m : map.values()) {
+            System.out.println(m);
+        }
+        System.out.println();
+
+        for(Integer m : map.keySet()) {
+            System.out.println(m + " " + map.get(m));
+        }
         System.out.println();
 
     }
